83806818aae8b7b1c810917f883051cc9f314a50
9620yao <15675456193@163.com>
2017-04-03 09:04:02 +0800

    我对UsersMapper做了添加 --姚秋林

--- findPartner/src/main/java/com/yc/ssm/mapper/UsersMapper.java
+++ findPartner/src/main/java/com/yc/ssm/mapper/UsersMapper.java
@@ -1,8 +1,13 @@
 package com.yc.ssm.mapper;
 
+import com.yc.ssm.entity.Users;
 
 public interface UsersMapper {
 
 	int insertUsers(String alid);
+
+	Users findUsersInfo(String alid);
+
+	int updateUserInfo(Users users);
 	
 }
