54942c5f2bc0be7faf492122269d46cc957396a8
9620yao <15675456193@163.com>
2017-04-03 09:06:04 +0800

    我对UsersServiceImpl做了添加  --姚秋林

--- findPartner/src/main/java/com/yc/ssm/service/impl/UsersServiceImpl.java
+++ findPartner/src/main/java/com/yc/ssm/service/impl/UsersServiceImpl.java
@@ -4,6 +4,7 @@ package com.yc.ssm.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.yc.ssm.entity.Users;
 import com.yc.ssm.mapper.UsersMapper;
 import com.yc.ssm.service.UsersService;
 
@@ -16,6 +17,16 @@ public class UsersServiceImpl implements UsersService {
 	@Override
 	public boolean insertUsers(String alid) {
 		return UsersMapper.insertUsers(alid)>0;
+	}
+
+	@Override
+	public Users listUsersInfos(String alid) {
+		return UsersMapper.findUsersInfo(alid);
+	}
+
+	@Override
+	public boolean modifyUserInfo(Users users) {
+		return UsersMapper.updateUserInfo(users)>0;
 	}
 
 }
