77f30b8b413017255f320a08a3325f04fed6d2b9
9620yao <15675456193@163.com>
2017-04-03 09:05:03 +0800

    我对UsersService做了添加 --姚秋林

--- findPartner/src/main/java/com/yc/ssm/service/UsersService.java
+++ findPartner/src/main/java/com/yc/ssm/service/UsersService.java
@@ -1,7 +1,13 @@
 package com.yc.ssm.service;
 
+import com.yc.ssm.entity.Users;
+
 public interface UsersService {
 
 	boolean insertUsers(String alid);
+
+	Users listUsersInfos(String alid);
+
+	boolean modifyUserInfo(Users users);
 
 }
