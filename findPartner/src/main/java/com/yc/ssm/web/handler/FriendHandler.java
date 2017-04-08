4aafba78d6eee9a776a9d88ef3d2183d7eef74c6
9620yao <15675456193@163.com>
2017-04-08 14:41:05 +0800

    去掉邮箱注册发送验证码的注释 --姚秋林

--- findPartner/src/main/java/com/yc/ssm/web/handler/FriendHandler.java
+++ findPartner/src/main/java/com/yc/ssm/web/handler/FriendHandler.java
@@ -2,7 +2,6 @@ package com.yc.ssm.web.handler;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
@@ -12,32 +11,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.yc.ssm.entity.Friends;
-import com.yc.ssm.entity.PaginationBean;
-import com.yc.ssm.entity.Speaks;
 import com.yc.ssm.entity.Users;
 import com.yc.ssm.service.FriendService;
 import com.yc.ssm.util.ServletUtil;
-
-
 
 @Controller("friendHandler")
 @RequestMapping("friend")
 public class FriendHandler {
 	@Autowired
 	private FriendService friendService;
-	
+
 	@RequestMapping(value = "list", method = RequestMethod.GET)
 	@ResponseBody
 	public List<Users> listFriends(HttpSession session) {
 		LogManager.getLogger().debug("我进来了 listFriends");
-		String aid=(String) session.getAttribute(ServletUtil.USERAID);
-		return friendService.listFriends("10000");
+		String aid = (String) session.getAttribute(ServletUtil.USERAID);
+		return friendService.listFriends(aid);
 	}
-
-	
-	
-
-	
 
 }
